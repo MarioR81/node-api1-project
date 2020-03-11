12740f9b8f29a8dc3e719ec86eb3440b773930b9
apple <apple@apples-MacBook-Pro-2.local>
2020-03-11 16:36:44 -0700

    added verification of data

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const shortid = require('shortid');
 
 const server = express();
 
-const users = [];
+const users = [
+    {
+        id: 1, // hint: use the shortid npm package to generate it
+        name: "Jane Doe", // String, required
+        bio: "Not Tarzan's Wife, another Jane",  // String, required
+    } 
+];
 
 
 server.use(express.json());
@@ -14,13 +20,44 @@ server.get('/', (req, res) => {
 
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
-    userInfo.id = shortid.generate();
-    users.push(userInfo);
-    res.status(201).json(userInfo)
-})
+
+    if (userInfo.name && userInfo.bio){
+        userInfo.id = shortid.generate();
+        users.push(userInfo);
+        res.status(201).json(userInfo)
+    } else if (!users){
+        res.status(500).json({ errorMessage: 'There was as error while saving the user tp the database'})
+    } else {
+        res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' })
+    }
+});
 
 server.get('/api/users', (req, res) => {
-    res.status(200).json(users)
+    if (users.length > 0){
+        res.status(200).json(users)
+    } else {
+        res.status(500).json({ errorMessage: 'Users infor could not be retrieved.' })
+    }
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = users.find(user => user.id === String(id))
+
+    if(!user) {
+        res.status(404).json({message: "user with given id does not exist"})
+    } else if (!id) {
+        res.status(500).json({message: "server error"})
+    } else {
+        res.status(200).json(user)
+    }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = users.find(user => user.id === String(id))
+    remove(user);
+    res.status(200).json('User Deleted Successfully.')
 });
 
 const PORT = 5000;
